386eacc2400e40a966cb4a2fdd137aa17a302aee
Нагимуллин Ильшат Рифатович <251911@mck-ktits.ru>
2023-02-01 18:19:37 +0300

    Add slider and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 const CartButton = () => {
     return (
-        <button>Корзина <span>1204 руб.</span></button>
+        <button>
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span>1204 руб.</span>
+        </button>
     )
 }
 
@@ -10,13 +14,22 @@ const Header = () => {
     return (
         <header className="header">
             <div className="logo">
+                <img src={logo} alt="React Snaeakers logo" />
 
+                <div>
+                    <h1>React Sneakers</h1>
+                    <p>Магазин лучших кроссовок</p>
+                </div>
             </div>
 
             <div className="nav">
                 <CartButton />
-                <NavLink to="/favorites">Избранное</NavLink>
-                <NavLink to="/orders">Заказы:</NavLink>
+                <NavLink to="/favorites">
+                    <i class="fa-solid fa-heart"></i>
+                </NavLink>
+                <NavLink to="/orders">
+                    <i class="fa-solid fa-user"></i>
+                </NavLink>
             </div>
         </header>
     )
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,24 @@
+import { children } from "react";
+
+const Slider = (children) => {
+    return (
+        <section className="slider">
+            <div className="wrapper">
+                <div className="slides">
+                    {children}
+                </div>
+                
+                <div className="controls">
+                    <button className="prev">
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </button>
+                    <button className="next">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Slider;
